12673ae34ea1f202c5ab4d3056aed063bef53600
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-10-24 15:17:11 +0300

    Initial Commit

---
+++ Product/ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductApp.swift
+//  Product
+//
+//  Created by Bekir Berke Yılmaz on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
